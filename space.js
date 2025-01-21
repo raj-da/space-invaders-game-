cf96dce6006477f1bccccac950e7776aa86f045e
Rebira Oli <rebiranegassa24@gmail.com>
2025-01-21 22:57:22 +0300

    Updated space.js

--- space.js
+++ space.js
@@ -1,1 +1,21 @@
+function shoot(e){
+    if(gameOver) return;
 
+    if(e.code == "Space"){
+        let bullet = {
+            x: ship.x + shipWidth*15/32,
+            y: ship.y,
+            width: tileSize/8,
+            height: tileSize/2,
+            used: false
+        }
+        bulletArray.push(bullet);
+    }
+}
+
+function detectCollision(a, b){
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
+}
